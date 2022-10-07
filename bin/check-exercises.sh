1bdf87142e38ee86bd056161b2c5dcc563028ed1
Luca Ferranti <49938764+lucaferranti@users.noreply.github.com>
2022-10-07 09:03:52 +0300

    nicer printing

--- bin/check-exercises.sh
+++ bin/check-exercises.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+
 cd exercises
 mkdir tmp-exercises
 
 cnt=0
 for exercise in practice/*/ concept/*/; do
     if [ -d "$exercise" ]; then
-        echo Testing $exercise
+        printf "\n======================================================================\nTESTING $exercise \n======================================================================\n"
 	mkdir -p tmp-exercises/$exercise
 	cp -a $exercise/. tmp-exercises/$exercise
 	cd tmp-exercises/$exercise
@@ -21,6 +24,8 @@ done
 rm -rf tmp-exercises
 
 if [ $cnt -gt 0 ]; then
-  echo $cnt tests failed
+  printf "\n${RED}$cnt TESTS FAILED\n"
   exit 1
+else
+  printf "\n${GREEN} ALL TESTS PASSED\n"
 fi
