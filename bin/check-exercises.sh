07dac3f4e415757cac4891d311a1d0a7f9af22c5
Luca Ferranti <49938764+lucaferranti@users.noreply.github.com>
2022-10-07 08:23:08 +0300

    exit with message 1 if tests fail

--- bin/check-exercises.sh
+++ bin/check-exercises.sh
@@ -3,6 +3,7 @@
 cd exercises
 mkdir tmp-exercises
 
+cnt=0
 for exercise in practice/*/ concept/*/; do
     if [ -d "$exercise" ]; then
         echo Testing $exercise
@@ -10,9 +11,16 @@ for exercise in practice/*/ concept/*/; do
 	cp -a $exercise/. tmp-exercises/$exercise
 	cd tmp-exercises/$exercise
 	cat .meta/example.chpl > src/*.chpl
-	mason test
+	if ! mason test ; then
+	  ((cnt++))
+	fi
 	cd -
     fi
 done
 
 rm -rf tmp-exercises
+
+if [ $cnt -gt 0 ]; then
+  echo $cnt tests failed
+  exit 1
+fi
